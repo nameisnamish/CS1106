6503d62a3a15d162e6d45c07cac7edfb9d0bb3ee
nameisnamish <namishmsasmyselfmein2033@gmail.com>
2025-04-10 18:43:44 +0530

    Create Userinfo.sh

---
+++ 3  set of practice questions/SET-2/Part B/Userinfo.sh
@@ -0,0 +1,19 @@
+# Displaying Username
+echo "Username:"
+whoami
+echo
+
+# Displaying Home Directory
+echo "Home Directory:"
+$HOME
+echo
+
+# Displaying Shell
+echo "Shell:"
+$SHELL
+echo
+
+# Displaying Group Memberships
+echo "Group Memberships:"
+groups
+echo
